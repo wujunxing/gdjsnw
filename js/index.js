5db4a93fb98d06b2c50567e871a04f49522db719
wujunxing <jmwujunxing@gmail.com>
2017-05-18 23:12:49 +0800

    0518

--- js/index.js
+++ js/index.js
@@ -1,12 +1,8 @@
 ﻿
-$(function () {
-    $("#flash_slider").tinycarousel({ interval: true, bullets: true });
-});
 
 $(function () {
-    $("#product_carousel").tinycarousel({ interval: true });
-    var tinycarousel_data = $("#product_carousel").data("plugin_tinycarousel");
-    $("#product_carousel").hover(
-        function () { tinycarousel_data.stop(); }, function () { tinycarousel_data.start(); }
-    )
+    $("#floatimg").adFloat({ width: 252, height: 172, top: 0, left: 0, step: 2 });
+    $("#closead").click(function () {
+        $("#floatimg").remove();
+    });
 });
